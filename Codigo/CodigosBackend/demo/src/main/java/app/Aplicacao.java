43d638dc7f4a723531d8e6e65d3841fe5a54f06b
Breno Pires Santos <123776858+brenodft@users.noreply.github.com>
2024-05-15 18:13:00 -0300

    Adicionados metodos CRUD para a classe

--- Codigo/CodigosBackend/demo/src/main/java/app/Aplicacao.java
+++ Codigo/CodigosBackend/demo/src/main/java/app/Aplicacao.java
@@ -1,5 +1,17 @@
 package app;
+import static spark.Spark.*;
+import service.MusicoService;
 
 public class Aplicacao {
+    public static MusicoService musicoService = new MusicoService();
 
+    public static void main(String[] args) {
+        port(6789);
+        staticFiles.location("/public");
+        post("/usuario/insert", (request, response) -> musicoService.insert(request, response));    
+        get("/usuario/get", (request, response) -> musicoService.get(request, response));
+        post("/usuario/update", (request, response) -> musicoService.update(request, response));
+        delete("/usuario/delete", (request, response) -> musicoService.delete(request, response));
+    }
 }
+
