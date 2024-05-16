236ecc8302e6268113440c1accfc1534c11e97f6
Arthur Xavier <kimadzn@gmail.com>
2024-05-16 18:32:59 -0300

    DAO commit conexao com BD

--- Codigo/CodigosBackend/demo/src/main/java/dao/DAO.java
+++ Codigo/CodigosBackend/demo/src/main/java/dao/DAO.java
@@ -3,45 +3,51 @@ package dao;
 import java.sql.*;
 
 public class DAO {
-	protected Connection conexao;
-	
-	public DAO() {
-		conexao = null;
-	}
-	
-	public boolean conectar() {
-		String driverName = "org.postgresql.Driver";                    
-		String serverName = "localhost";
-		String mydatabase = "teste";
-		int porta = 5432;
-		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
-		String username = "ti2cc";
-		String password = "ti@cc";
-		boolean status = false;
-
-		try {
-			Class.forName(driverName);
-			conexao = DriverManager.getConnection(url, username, password);
-			status = (conexao == null);
-			System.out.println("Conexão efetuada com o postgres!");
-		} catch (ClassNotFoundException e) { 
-			System.err.println("Conexão NÃO efetuada com o postgres -- Driver não encontrado -- " + e.getMessage());
-		} catch (SQLException e) {
-			System.err.println("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
-		}
-
-		return status;
-	}
-	
-	public boolean close() {
-		boolean status = false;
-		
-		try {
-			conexao.close();
-			status = true;
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		return status;
-	}
+    protected Connection conexao;
+    
+    public DAO() {
+        conexao = null;
+    }
+    
+    public boolean conectar() {
+        String driverName = "org.postgresql.Driver";                    
+        String serverName = "central-do-musico-bd.postgres.database.azure.com";
+        String mydatabase = "centraldomusico";
+        int porta = 5432;
+        String url = "jdbc:postgresql://" + serverName + ":" + porta + "/" + mydatabase;
+        String username = "JesusCristo";
+        String password = "A4b3c2d1";
+        boolean status = false;
+    
+        try {
+            Class.forName(driverName);
+            conexao = DriverManager.getConnection(url, username, password);
+            if (conexao != null) {
+                status = true;
+                System.out.println("Conexão bem-sucedida!");
+            } else {
+                System.out.println("Falha na conexão.");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver não foi encontrado: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
+    
+        return status;
+    }
+    
+    public boolean close() {
+        boolean status = false;
+        
+        if (conexao != null) {
+            try {
+                conexao.close();
+                status = true;
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return status;
+    }
 }
