c19666a9368b445762bd70f9428d0f6c43932082
Breno Pires Santos <123776858+brenodft@users.noreply.github.com>
2024-05-21 19:19:56 -0300

    tratando JSON

--- Codigo/CodigosBackend/demo/src/main/java/service/MusicoService.java
+++ Codigo/CodigosBackend/demo/src/main/java/service/MusicoService.java
@@ -4,7 +4,8 @@ import dao.MusicoDAO;
 import model.Musico;
 import spark.Request;
 import spark.Response;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.util.List;
 
 public class MusicoService {
@@ -19,15 +20,27 @@ public class MusicoService {
             String body = req.body();
             System.out.println("Request Body: " + body); // Log the request body for debugging
 
-            String nome = req.queryParams("nome");
-            String descricao = req.queryParams("descricao");
-            String senha = req.queryParams("senha");
-            float cache = Float.parseFloat(req.queryParams("cache"));
-            String instrumento1 = req.queryParams("instrumento1");
-            String instrumento2 = req.queryParams("instrumento2");
-            String instrumento3 = req.queryParams("instrumento3");
-            String objetivo = req.queryParams("objetivo");
-            String estilo = req.queryParams("estilo");
+            // Parse the JSON body to get the parameters
+            JsonObject json = JsonParser.parseString(body).getAsJsonObject();
+            String nome = json.get("nome").getAsString();
+            String descricao = json.get("descricao").getAsString();
+            String senha = json.get("senha").getAsString();
+            float cache = json.get("cache").getAsFloat();
+            String instrumento1 = json.get("instrumento1").getAsString();
+            String instrumento2 = json.get("instrumento2").getAsString();
+            String instrumento3 = json.get("instrumento3").getAsString();
+            String objetivo = json.get("objetivo").getAsString();
+            String estilo = json.get("estilo").getAsString();
+
+            System.out.println("Nome: " + nome);
+            System.out.println("Descricao: " + descricao);
+            System.out.println("Senha: " + senha);
+            System.out.println("Cache: " + cache);
+            System.out.println("Instrumento1: " + instrumento1);
+            System.out.println("Instrumento2: " + instrumento2);
+            System.out.println("Instrumento3: " + instrumento3);
+            System.out.println("Objetivo: " + objetivo);
+            System.out.println("Estilo: " + estilo);
 
             Musico musico = new Musico(0, nome, descricao, senha, cache, instrumento1, instrumento2, instrumento3, objetivo, estilo);
             System.out.println("Musico Object: " + musico); // Log the Musico object for debugging
