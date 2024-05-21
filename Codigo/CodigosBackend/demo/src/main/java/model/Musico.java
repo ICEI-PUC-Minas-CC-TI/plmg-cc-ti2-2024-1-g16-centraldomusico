cc122a088f5660b343d58d68aeb3ab8b8650a345
Breno Pires Santos <123776858+brenodft@users.noreply.github.com>
2024-05-21 19:00:43 -0300

    Add files via upload

--- Codigo/CodigosBackend/demo/src/main/java/model/Musico.java
+++ Codigo/CodigosBackend/demo/src/main/java/model/Musico.java
@@ -1,112 +1,108 @@
 package model;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-
 public class Musico {
-	private int id;
-	private String descricao;
-	private String nomeCompleto;
-	private String senha;
-	private float cache;
-	private String instrumento;
-	private String objetivo;
-	private String estilo;
+    private int id;
+    private String nome;
+    private String descricao;
+    private String senha;
+    private float cache;
+    private String instrumento1;
+    private String instrumento2;
+    private String instrumento3;
+    private String objetivo;
+    private String estilo;
 
-	
-	public Musico() {
-		id = -1;
-		descricao = "";
-		cache = 0.00F;
-		nomeCompleto = "";
-		senha = "";
-		instrumento = "";
-		objetivo = "";
-		estilo = "";
-	}
+    public Musico(int id, String nome, String descricao, String senha, float cache, String instrumento1, String instrumento2, String instrumento3, String objetivo, String estilo) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.senha = senha;
+        this.cache = cache;
+        this.instrumento1 = instrumento1;
+        this.instrumento2 = instrumento2;
+        this.instrumento3 = instrumento3;
+        this.objetivo = objetivo;
+        this.estilo = estilo;
+    }
 
-	public Musico(int id, String nome,String senha,String objetivo,String instrumento,String descricao, float cache,String estilo) {
-		setId(id);
-		setNome(nome);
-		setSenha(senha);
-		setDescricao(descricao);
-		setCache(cache);
-		setInstrumento(instrumento);
-		setObjetivo(objetivo);
-		setEstilo(estilo);
-	}		
-	
-	public int getID() {
-		return id;
-	}
-	public float getCache() {
-		return cache;
-	}
-	public String getInstrumento() {
-		return instrumento;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public String getObjetivo() {
-		return objetivo;
-	}
-	public String getNome() {
-		return nomeCompleto;
-	}
-	public String getEstilo() {
-		return estilo;
-	}
-	
-	public void setCache(float cache) {
-		this.cache = cache;
-	}
-	
-	public void setObjetivo(String objetivo) {
-		this.objetivo = objetivo;
-	}
-	public void setInstrumento(String instrumento) {
-		this.instrumento = instrumento;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public void setNome(String name) {
-		this.nomeCompleto = name;
-	}
-	public void setEstilo(String estilo) {
-		this.estilo = estilo;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
 
-	
-	public String getDescricao() {
-		return descricao;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getDescricao() {
+        return descricao;
+    }
 
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
-	/**
-	 * Método sobreposto da classe Object. É executado quando um objeto precisa
-	 * ser exibido na forma de String.
-	 */
-	@Override
-	public String toString() {
-		return "Musico: " + nomeCompleto + "   Cache: R$" + cache + "   Estilo: " + estilo + "   Instrumento: "
-				+ instrumento  + "   Descrição: " + descricao+ "   Objetivo: "+ objetivo;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return (this.getID() == ((Musico) obj).getID());
-	}	
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public float getCache() {
+        return cache;
+    }
+
+    public void setCache(float cache) {
+        this.cache = cache;
+    }
+
+    public String getInstrumento1() {
+        return instrumento1;
+    }
+
+    public void setInstrumento1(String instrumento1) {
+        this.instrumento1 = instrumento1;
+    }
+
+    public String getInstrumento2() {
+        return instrumento2;
+    }
+
+    public void setInstrumento2(String instrumento2) {
+        this.instrumento2 = instrumento2;
+    }
+
+    public String getInstrumento3() {
+        return instrumento3;
+    }
+
+    public void setInstrumento3(String instrumento3) {
+        this.instrumento3 = instrumento3;
+    }
+
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(String objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
 }
