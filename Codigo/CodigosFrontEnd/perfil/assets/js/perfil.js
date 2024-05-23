11057a1bae1eba5e190c07e75c730827c438e07e
Breno Pires Santos <123776858+brenodft@users.noreply.github.com>
2024-05-23 18:06:50 -0300

    agora ele "lembra" se vc fez login ou nao

--- Codigo/CodigosFrontEnd/perfil/assets/js/perfil.js
+++ Codigo/CodigosFrontEnd/perfil/assets/js/perfil.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    //esconder id da url
+    const id = localStorage.getItem('id');
+    const token = localStorage.getItem('token');
+    //armazenar id no local storage
+    console.log('TOKEN:', token);
+    // Esconder ID da URL
     window.history.replaceState({}, document.title, "/Codigo/CodigosFrontEnd/perfil/perfil.html");
 
-    if (id) {
+    if (token) {
+        console.log('token checado');
+        console.log('ID:', id);
         fetch(`http://localhost:6789/usuario/get/perfil?id=${id}`, {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
             }
         })
         .then(response => {
@@ -28,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById('instrumento3').textContent = data.instrumento3;
                 document.getElementById('objetivo').textContent = data.objetivo;
                 document.getElementById('estilo').textContent = data.estilo;
+
             }
         })
         .catch(error => {
@@ -35,6 +42,13 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('msgError').textContent = 'Erro ao carregar os dados do usuário. Tente novamente.';
         });
     } else {
-        document.getElementById('msgError').textContent = 'ID de usuário ausente na URL.';
+        document.getElementById('msgError').textContent = 'Token de autenticação ausente. Faça login novamente.';
     }
 });
+
+//criar funcao onclick para logout
+function logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('id');
+    window.location.href = '/Codigo/CodigosFrontEnd/Login/novologin.html';
+}
