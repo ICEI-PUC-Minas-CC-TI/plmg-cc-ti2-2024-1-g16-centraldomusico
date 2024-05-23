a1783bdbbd32789417caab33fbc2c014680d2137
Breno Pires Santos <123776858+brenodft@users.noreply.github.com>
2024-05-23 17:14:50 -0300

    conexao com bd

--- Codigo/CodigosFrontEnd/Login/assets/js/login.js
+++ Codigo/CodigosFrontEnd/Login/assets/js/login.js
@@ -1,33 +1,29 @@
+//criar id randomico
 var id;
 function entrar() {
-    const usuario = document.getElementById('login').value;
-    const senha = document.getElementById('senha').value;
+    var usuario = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
 
-    fetch('https://jsonservercentraldomusico.arthurcarvalh19.repl.co/perfil')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao obter os dados dos usuários');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const user = data.find(user => user.usuario === usuario && user.senha === senha);
-
-            if (user) {
-                alert('Login bem-sucedido');
-                // Redirecionar o usuário para explorar.html no lado do servidor
-                window.location.href = "/perfil/perfil.html?id=" + user.id;
-                id = user.id;
-            } else {
-                document.getElementById('msgError').innerText = 'Credenciais inválidas';
-            }
-        })
-        .catch(error => {
-            console.error('Erro na requisição fetch:', error);
-            document.getElementById('msgError').innerText = 'Erro de comunicação com o servidor';
-        });
-
-    return false; // Evita o envio do formulário tradicional
+    //método GET para fazer login
+    fetch(`http://localhost:6789/usuario/get?usuario=${usuario}&senha=${senha}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro na requisição: ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Resposta do servidor:', data);
+        id = data.id;
+        window.location.href = `/Codigo/CodigosFrontEnd/perfil/perfil.html?id=${id}`;
+    })
+    .catch(error => {
+        console.error('Erro ao fazer login:', error);
+        document.getElementById('msgError').textContent = 'Usuário ou senha incorretos. Tente novamente.';
+    });
 }
-
-
