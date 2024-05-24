5caf732eb95947717eee5e093009ebff2ae75831
DONINHAPIPOCO <lfrodaviva@hotmail.com>
2024-05-23 21:09:50 -0300

    Alterações estéticas simples

--- Codigo/CodigosFrontEnd/cadastroperfil/assets/js/script.js
+++ Codigo/CodigosFrontEnd/cadastroperfil/assets/js/script.js
@@ -38,24 +38,32 @@ function enviarDados() {
     };
 
     fetch('http://localhost:6789/usuario/insert', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(perfil)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro na requisição: ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        alert('Perfil criado com sucesso!');
-        window.location.href = '/Codigo/CodigosFrontEnd/Login/novologin.html';
-        console.log('Resposta do servidor:', data);
-    })
-    .catch(error => {
-        console.error('Erro durante a requisição fetch:', error);
-    });
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(perfil)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na requisição: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('Perfil criado com sucesso!');
+            window.location.href = '/Codigo/CodigosFrontEnd/Login/novologin.html';
+            console.log('Resposta do servidor:', data);
+        })
+        .catch(error => {
+            console.error('Erro durante a requisição fetch:', error);
+        });
 }
+
+document.getElementById('cache').addEventListener('input', function(e) {
+    let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não é dígito
+    value = (value / 100).toFixed(2); // Divide por 100 para ter duas casas decimais
+    value = value.replace('.', ','); // Substitui ponto por vírgula
+    value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Insere pontos a cada 3 dígitos
+    e.target.value = 'R$' + value;
+});
